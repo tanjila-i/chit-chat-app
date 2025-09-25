6483cceb36e2459f3127b6420581fcbece78762c
Tanjila Islam <tanjila.info90@gmail.com>
2025-09-25 10:18:49 +0600

    pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="bg-[#050814] min-h-screen">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
